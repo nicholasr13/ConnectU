2d1e1847b76c200b2623900c3ab8e8e6e997079e
nicholasr13 <nicholasragoonanan02@gmail.com>
2021-04-25 02:43:01 +0000

    first commit

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -8,6 +8,8 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+ENV = 'production'
+SQLITEDB = False
 db= SQLAlchemy(app)
 
 bcrypt = Bcrypt(app)
